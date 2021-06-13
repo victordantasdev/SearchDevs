6782b4c8b05ed31f4e85b4371e6d16d589b37380
joaovictordantasj <joaovictordjardim@gmail.com>
2021-06-13 00:27:53 -0300

    Error page configurated

--- src/pages/ErrorPage.js
+++ src/pages/ErrorPage.js
@@ -1,3 +1,59 @@
-export default function Error() {
-  return <div>Error Page</div>;
+import { Icon } from '@material-ui/core';
+import { Helmet } from 'react-helmet';
+import Lottie from 'react-lottie';
+import { useHistory } from 'react-router';
+import styled from 'styled-components';
+import animationData from '../assets/animations/errorAnimation.json';
+import { Button } from '../styles/SearchStyles';
+
+const Container = styled.div`
+  margin-top: 10%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  align-content: center;
+`;
+
+export default function ErrorPage() {
+  const history = useHistory();
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    history.push('/');
+  };
+
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: animationData,
+  };
+
+  return (
+    <>
+      <Helmet>
+        <link
+          rel='icon'
+          href='https://images.vexels.com/media/users/3/132068/isolated/preview/f9bb81e576c1a361c61a8c08945b2c48-iacute-cone-de-pesquisa-by-vexels.png'
+        />
+        <title>Search Devs | 404</title>
+      </Helmet>
+      <Container>
+        <Lottie
+          options={defaultOptions}
+          width={400}
+          height={400}
+          isStopped={false}
+          isPaused={false}
+        />
+        <h1>Oops...</h1>
+        <h2>
+          Unfortunately the name of this dev was not found on github, please go
+          back to the home page and try again.
+        </h2>
+        <Button onClick={handleSubmit} style={{ 'margin-top': '20px' }}>
+          <Icon>arrow_back</Icon>Back
+        </Button>
+      </Container>
+    </>
+  );
 }

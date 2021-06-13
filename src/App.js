a36622511704a6986333c1db5be84ec2f4e3f0bf
joaovictordantasj <joaovictordjardim@gmail.com>
2021-06-12 23:21:36 -0300

    Routes created

--- src/App.js
+++ src/App.js
@@ -1,3 +1,24 @@
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import ErrorPage from './pages/ErrorPage';
+import Home from './pages/Home';
+import Profile from './pages/Profile';
+
 export default function App() {
-  return <div>Search Devs</div>;
+  return (
+    <Router>
+      <Switch>
+        <Route exact path='/'>
+          <Home />
+        </Route>
+
+        <Route path='/profile/:username'>
+          <Profile />
+        </Route>
+
+        <Route path='/404'>
+          <ErrorPage />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
